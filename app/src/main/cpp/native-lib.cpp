2f9266189c8f14a9e09f92a8cfa5a91cde19a58d
elementarymacosx <elementarymacosx@gmail.com>
2019-10-25 01:49:09 +0630

    Htin

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,9 +2,38 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_mycapplication_MainActivity_stringFromJNI(
+Java_com_example_mycapplication_MainActivity_stringFromJni(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_mycapplication_MainActivity_add(
+        JNIEnv *env,
+jobject, jint x, jint y /* this */) {
+
+return x + y;
+}
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_mycapplication_MainActivity_sub(
+        JNIEnv *env,
+        jobject, jint x, jint y /* this */) {
+
+return x - y;
+}
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_mycapplication_MainActivity_multiply(
+        JNIEnv *env,
+        jobject, jint x, jint y /* this */) {
+
+return x * y;
+}
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_mycapplication_MainActivity_divide(
+        JNIEnv *env,
+        jobject, jint x, jint y /* this */) {
+
+return x / y;
+}
+
--- app/src/main/java/com/example/mycapplication/MainActivity.java
+++ app/src/main/java/com/example/mycapplication/MainActivity.java
@@ -5,26 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     // Used to load the 'native-lib' library on application startup.
-    static {
+    static
+    {
         System.loadLibrary("native-lib");
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText(stringFromJni());
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String stringFromJni();
+    public native int add();
+    public native int sub();
+    public native int multiply();
+    public native int divide();
+
 }
