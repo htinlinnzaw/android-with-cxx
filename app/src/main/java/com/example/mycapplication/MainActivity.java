bf1abce5f8bcd6758a3538ed727a60a23992430b
Htin Linn Zaw <htinlinnzaw.dev@gmail.com>
2020-06-22 15:30:50 +0630

    add: AsyncTask memory leaks code

--- app/src/main/java/com/example/mycapplication/MainActivity.java
+++ app/src/main/java/com/example/mycapplication/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.mycapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,6 +27,22 @@ public class MainActivity extends AppCompatActivity
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJni());
+    }
+
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        new AsyncTask<Void, Void, Void>() { // These AsyncTask would cause Memory Leaks
+            @Override
+            protected Void doInBackground(Void... voids) {
+                while (true) {
+                    Log.e("AsyncTask", "count: " + counter.get());
+                    counter.incrementAndGet();
+                }
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     /**
